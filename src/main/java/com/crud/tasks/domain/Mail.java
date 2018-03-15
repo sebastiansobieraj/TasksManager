98a78c961cb50067d264b649b1ef6d0afb334280
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2018-03-15 21:51:18 +0100

    add cc option to email sender v2

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -2,12 +2,17 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Mail {
+    @NonNull
     private String mailTo;
+    @NonNull
     private String subject;
+    @NonNull
     private String message;
     private String toCc;
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -35,12 +35,10 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(mail.getToCc() == null) {
-            return mailMessage;
-        } else {
+        if (mail.getToCc() != null) {
             mailMessage.setCc(mail.getToCc());
-            return mailMessage;
         }
+        return mailMessage;
     }
 }
 
